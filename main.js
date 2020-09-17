92a980f984e87bf8aa90b22d0f4dbc01c8d4b174
DmytroZubkov <dmytro.p.zubkov@gmail.com>
2020-09-17 12:57:19 +0300

    Added optgroups in lenses dropdown

--- main.js
+++ main.js
@@ -1,25 +1,55 @@
 const HOUSING_OPTIONS = [
-    {id: "C" , label: "Console"},
-    {id: "S" , label: "Suspended"},
+    { id: "C", label: "Console" },
+    { id: "S", label: "Suspended" }
 ];
 
 const MODULE_QTY_OPTIONS = [
-    {id: "1" , label: "1 - module"},
-    {id: "2" , label: "2 - module"},
-    {id: "3" , label: "3 - module"},
-    {id: "4" , label: "4 - module"},
-    {id: "5" , label: "5 - module"},
+    { id: "1", label: "1 - module" },
+    { id: "2", label: "2 - module" },
+    { id: "3", label: "3 - module" },
+    { id: "4", label: "4 - module" },
+    { id: "5", label: "5 - module" }
 ];
 
-const MODULE_POWER_OPTIONS = [
-    {id: "20" , label: "20 W"},
-    {id: "30" , label: "30 W"},
-    {id: "40" , label: "40 W"},
-    {id: "60" , label: "60 W"},
-    {id: "80" , label: "80 W"},
+const MODULE_POWER_OPTIONS = [{ id: "20", label: "20 W" },
+    { id: "30", label: "30 W" },
+    { id: "40", label: "40 W" },
+    { id: "60", label: "60 W" },
+    { id: "80", label: "80 W" }
 ];
 
-https://stackoverflow.com/questions/22266171/javascript-html-select-add-optgroup-and-option-dynamically
+const LENS_OPTIONS = [
+    //COB Street Optics
+    { optgr: "COB Street Optics", id: "S1", label: "S1 (145°x65°)", ldc: "" },
+    { optgr: "COB Street Optics", id: "S2", label: "S2 (170°x120°)", ldc: "" },
+    { optgr: "COB Street Optics", id: "S3", label: "S3 (160°x80°)", ldc: "" },
+    { optgr: "COB Street Optics", id: "S4", label: "S4 (150°x70°)", ldc: "" },
+    //SMD-28 Street Optics
+    { optgr: "SMD-28 Street Optics", id: "S6/T1", label: "S6/T1 - type I (145°x80°)", ldc: "" },
+    { optgr: "SMD-28 Street Optics", id: "S6/T2", label: "S6/T2 - type II (160°x50°)", ldc: "" },
+    { optgr: "SMD-28 Street Optics", id: "S6/T3", label: "S6/T3 - type III (150°x60°)", ldc: "" },
+    { optgr: "SMD-28 Street Optics", id: "S6/T5s", label: "S6/T5s - type V (140°)", ldc: "" },
+    { optgr: "SMD-28 Street Optics", id: "S6/Sc", label: "S6/Sc - type II/III (long, 160°x65°)", ldc: "" },
+    //COB Floodlight Optics
+    { optgr: "COB Floodlight Optics", id: "F1/45", label: "F1/45 (45°x45°)", ldc: "" },
+    { optgr: "COB Floodlight Optics", id: "F1/60", label: "F1/60 (60°x60°)", ldc: "" },
+    { optgr: "COB Floodlight Optics", id: "F1/90", label: "F1/90 (90°x90°)", ldc: "" },
+    { optgr: "COB Floodlight Optics", id: "F1/120", label: "F1/120 (120°x120°)", ldc: "" },
+    //SMD-28 Floodlight Optics
+    { optgr: "SMD-28 Floodlight Optics", id: "F6/30", label: "F6/30 (30°x30°)", ldc: "" },
+    { optgr: "SMD-28 Floodlight Optics", id: "F6/65", label: "F6/65 (65°x65°)", ldc: "" },
+    { optgr: "SMD-28 Floodlight Optics", id: "F6/90", label: "F6/90 (90°x90°)", ldc: "" },
+    //SMD-36 Floodlight Optics
+    { optgr: "SMD-36 Floodlight Optics", id: "F7/18", label: "F7/18 (18°x18°)", ldc: "" },
+    { optgr: "SMD-36 Floodlight Optics", id: "F7/22", label: "F7/22 (22°x22°)", ldc: "" },
+    { optgr: "SMD-36 Floodlight Optics", id: "F7/30", label: "F7/30 (30°x30°)", ldc: "" },
+    { optgr: "SMD-36 Floodlight Optics", id: "F7/45", label: "F7/45 (45°x45°)", ldc: "" },
+    { optgr: "SMD-36 Floodlight Optics", id: "F7/1840", label: "F7/1840 (18°x40°)", ldc: "" },
+    { optgr: "SMD-36 Floodlight Optics", id: "F7/115d", label: "F7/115d (115°x115°)", ldc: "" },
+    { optgr: "SMD-36 Floodlight Optics", id: "F7/125", label: "F7/125 (125°x125°)", ldc: "" }
+];
+
+
 
 let selHousing = document.getElementById('housing');
 let selModuleQty = document.getElementById('module-qty');
@@ -37,7 +67,7 @@ let selAscType = document.getElementById('asc');
 document.addEventListener('DOMContentLoaded', init, false);
 
 function init() {
-    
+
     HOUSING_OPTIONS.forEach((el, index) => {
         let opt = document.createElement("option");
         opt.value = el.id;
@@ -59,6 +89,24 @@ function init() {
         selModPower.add(opt);
     });
 
+    LENS_OPTIONS.forEach((el, index) => {
+        // console.log(
+        //     index,
+        //     index-1,
+        //     LENS_OPTIONS[index === 0 ? 0 : index-1].optgr !== LENS_OPTIONS[(index)].optgr,
+        //     );
+
+        let gr = document.createElement("optgroup");
+            if (LENS_OPTIONS[index === 0 ? 0 : index - 1].optgr !== LENS_OPTIONS[(index)].optgr || index === 0) {
+                gr.label = el.optgr;
+                selLensType.add(gr);
+            };
+        
+        let opt = document.createElement("option");
+        opt.value = el.id;
+        opt.text = el.label;
+        selLensType.add(opt);
+    });
 
 };
 
@@ -78,11 +126,11 @@ function singleSelectChangeText() {
     let selStdLetterTypeVal = selStdLetterType.options[selStdLetterType.selectedIndex].value;
     let selManufTypeVal = selManufType.options[selManufType.selectedIndex].value;
     let selSpcTypeVal = selSpcType.options[selSpcType.selectedIndex].value;
-        let selSpcTypeTxt = selSpcType.options[selSpcType.selectedIndex].text;
+    let selSpcTypeTxt = selSpcType.options[selSpcType.selectedIndex].text;
     let selDimTypeVal = selDimType.options[selDimType.selectedIndex].value;
-        let selDimTypeTxt = selDimType.options[selDimType.selectedIndex].text;
+    let selDimTypeTxt = selDimType.options[selDimType.selectedIndex].text;
     let selMountTypeVal = selMountType.options[selMountType.selectedIndex].value;
-        let selMountTypeTxt = selMountType.options[selMountType.selectedIndex].text;
+    let selMountTypeTxt = selMountType.options[selMountType.selectedIndex].text;
     let selAscTypeVal = selAscType.options[selAscType.selectedIndex].value;
 
 
@@ -103,7 +151,7 @@ function singleSelectChangeText() {
     const SKU3 = ((selMountTypeVal === "") ? "" : "-") + selMountTypeVal;
     const SKU4 = ((selAscTypeVal === "") ? "" : " + ") + selAscTypeVal;
 
-    
+
 
 
     let cmoboSKU = document.getElementById("combo-sku");
@@ -334,7 +382,7 @@ function searchSku() {
     let req = input.value;
     console.log(req);
 
-    if ( req === "" ) {
+    if (req === "") {
         console.log(true);
     } else {
         console.log(false);
